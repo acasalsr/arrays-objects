c228e16a8c18e3f8d91758b8792a3853ca91257a
andreu casals <acasalsr@gmail.com>
2022-02-23 20:01:49 +0100

    exercici 2

---
+++ exercici2.js
@@ -0,0 +1,8 @@
+/*
+Afegir un element.
+Donat un array d’enters, que puguis afegir un nou enter tot indicant a quina posició el vols afegir.
+*/
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr.splice(3, 0, 121);
+console.log(arr);
